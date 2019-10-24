f66b74d9fdbbcbab4e2617ae429f2069fc3edd9e
Sriram R <sriram@shikshalokam.org>
2019-10-24 15:42:59 +0530

    config changes to suit build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,7 +2,6 @@ var { dev, prod } = require('@ionic/app-scripts/config/webpack.config.js');
 
 const path = require('path');
 const webpackMerge = require('webpack-merge');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const customConfig = {
   resolve: {
